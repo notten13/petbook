1d905e615e927642a021701f82f0428ad009986d
notten13 <15044786+notten13@users.noreply.github.com>
2023-03-11 19:03:08 +0000

    feat: sign up form (wip)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user_params = params.require(:user).permit(
+      :username,
+      :email,
+      :password,
+      :password_confirmation
+    )
+
+    @user = User.new(user_params)
+
+    if @user.valid?
+      render 'new'
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+end
